d0f149208489be73b9156c6f1a22397e4f346041
7ras0tresh <maximandreychuk1999@gmail.com>
2023-11-17 12:52:29 +0300

    add problem

--- find_all_just_multipliers_of_number/find_all_just_multipliers_of_number.py
+++ find_all_just_multipliers_of_number/find_all_just_multipliers_of_number.py
@@ -1,17 +1,12 @@
+"""Создаём массив из чисел от 2 до загаданного числа."""
 def decomposition_Y(Y):
     arr = []
     for i in range(2,Y+1):
         arr.append(i)
-    print(arr)
+    # print(arr)
     return arr
 
-# def find_multipliers_Y(arr):
-#     n_arr = []
-#     for x,y in arr:
-#         if x*y==arr[-1]:
-#             arr.append(x)
-#     print(n_arr)
-
+"""Находим множители."""
 def find_multipliers_Y(arr):
     new_arr = []
     inx = 0
@@ -23,17 +18,19 @@ def find_multipliers_Y(arr):
                 new_arr.append(el)
         arr.pop(0)
     new_arr.sort()
-    print(new_arr)
+    # print(new_arr)
     return new_arr
 
+"""Удаляем дубликаты."""
 def delete_double_x(arr):
     new_arr = []
     for i in arr:
         if i not in new_arr:
             new_arr.append(i)
-    print(new_arr)
+    # print(new_arr)
     return new_arr
-    
+
+"""Находим простые множители."""
 def find_just_x_in_arr(arr):
     answer_arr = []
     for x in arr:
@@ -47,11 +44,8 @@ def find_just_x_in_arr(arr):
         print(f'Ответ: {answer_arr}')
     else: print('У этого числа нет простых множителей кроме, возможно, самого числа')
 
-
-# mul(decomposition_Y(40))
-# find_multipliers_Y(decomposition_Y(40))
-# Y = int(input())
-find_just_x_in_arr(delete_double_x(find_multipliers_Y(decomposition_Y(int(input())))))
+if __name__ == '__main__':
+    find_just_x_in_arr(delete_double_x(find_multipliers_Y(decomposition_Y(int(input())))))
 
 
 
